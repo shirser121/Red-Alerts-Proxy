70b2a0354799ef77e03b3a7096a18f49388dea60
Michal Serlui <70711723+shirser121@users.noreply.github.com>
2023-10-18 13:26:57 +0300

    Error if API_URL is not set

--- red_alerts/__init__.py
+++ red_alerts/__init__.py
@@ -2,11 +2,18 @@ from flask import Flask, jsonify, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from dotenv import load_dotenv
+
 import json
 
 from red_alerts.logger import logger
 from red_alerts.shared import redis_client
 from red_alerts.routes import init_routes
+
+load_dotenv()
+API_URL = os.environ.get("API_URL")
+if not API_URL:
+    raise Exception("API_URL is not set")
 
 app = Flask(__name__)
 limiter = Limiter(
